44eab1dc386af5febfdb0cee8834dd3569d0c78d
Tatiana freitas <tfsouza@gmail.com>
2012-02-14 19:38:33 -0200

    Criacao do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termobra::Application.config.secret_token = '1cdf1c75ff6f15b997ce6043bfec320fac113f5968ab305a9cabebfd10c0e4da684fb076dd478aec265db204bb9f01b98cde72415ed7b97a4aa2b426e9aa69b4'
